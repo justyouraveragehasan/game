f7c01d87923dc8303fb1a0b9b6959cb037accf26
justyouraveragehasan <hasanali.soda@gmail.com>
2024-06-12 13:59:02 +0100

    added shell for linux dev

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,26 @@
 import pygame
 
+def game() -> None:
+
+    WIDTH = 360
+    HEIGHT = 480
+    FPS = 30
+
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH,HEIGHT))
+    pygame.display.set_caption("Game")
+    clock = pygame.time.Clock()
+    
+    while True:
+        clock.tick()
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
+
 def main() -> None:
     print("Hello World!")
+    game()
     return None
 
 if __name__ == "__main__":
