8f7e8087ec758349febed3fcd61012c026ddaa39
justyouraveragehasan <hasanali.soda@gmail.com>
2024-06-14 12:29:06 +0100

    more shit

--- core/graphics.py
+++ core/graphics.py
@@ -1,20 +1,13 @@
 import pygame
 
-class Button:
-    def __init__(self):
-        pass
-
-    def draw(self):
-        pass
-
-
 def main_menu(screen,WIDTH,HEIGHT) -> None:
     bg = pygame.image.load('resources/main_menu_bg.png')
     bg = pygame.transform.scale(bg,(WIDTH,HEIGHT))
 
     screen.blit(bg, (0,0))
     #test button
-    pygame.draw.rect(screen,(255,0,255),[WIDTH/2 -200,HEIGHT/2,400,100])
+    button1 = pygame.Rect(WIDTH/2 -200,HEIGHT/2,400,100)
+    pygame.draw.rect(screen,(255,0,255),button1)
 
 
 def game_screen(screen,WIDTH,HEIGHT) -> None:
