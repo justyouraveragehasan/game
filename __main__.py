670616254420000ba06d23c3a9c428d584e5d976
justyouraveragehasan <hasanali.soda@gmail.com>
2024-06-15 15:29:35 +0100

    much better

--- __main__.py
+++ __main__.py
@@ -2,41 +2,24 @@ import pygame, sys
 
 from core import graphics
 
+#initial pygame init shit
+WIDTH = 1920
+HEIGHT = 1080
+FPS = 30
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Game")
+clock = pygame.time.Clock()
+
 def main_menu() -> None:
-    pass 
-
-def game() -> None:
-    #initial pygame init shit
-    WIDTH = 1920
-    HEIGHT = 1080
-    FPS = 30
-
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
-    pygame.display.set_caption("Game")
-    clock = pygame.time.Clock()
-   
-    # Gameloop 
-    run = True
-
-    state = "Game" 
-    state = "Menu"
-
-    while run:
-        #set fps
+    while True:
         clock.tick(FPS)
-
-        #draw screen
-        match state:
-            case "Menu":
-                graphics.draw_main_menu(screen,WIDTH,HEIGHT)
-                button1 = pygame.Rect(WIDTH/2 -200,HEIGHT/2,400,100)
-                pygame.draw.rect(screen,(255,0,255),button1)
-            case "Game":
-                graphics.draw_game_screen(screen,WIDTH,HEIGHT)
-            case _:
-                print("How did we get here?!")
+        graphics.draw_main_menu(screen,WIDTH,HEIGHT)
+        button1 = pygame.Rect(WIDTH/2 -200,HEIGHT/2,400,100)
+        pygame.draw.rect(screen,(255,0,255),button1)
         
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -44,12 +27,28 @@ def game() -> None:
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if button1.collidepoint(event.pos):
-                    state = "Game"
+                    game()
+
+        pygame.display.flip()
+
+    
+
+def game() -> None:
+    running = True
+    while running:
+        clock.tick(FPS)
+        graphics.draw_game_screen(screen,WIDTH,HEIGHT)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
         pygame.display.flip()
 
 
+
 if __name__ == "__main__":
-    game()
+    main_menu()
 
 
