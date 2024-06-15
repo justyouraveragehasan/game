dcdf234a2bb2707f36868349181ee008f9b4d86e
justyouraveragehasan <hasanali.soda@gmail.com>
2024-06-15 23:21:48 +0100

    proof of concept

--- core/graphics.py
+++ core/graphics.py
@@ -9,6 +9,8 @@ def draw_main_menu(screen,WIDTH,HEIGHT) -> None:
 
 
 def draw_game_screen(screen,WIDTH,HEIGHT) -> None:
-    bg = pygame.image.load('resources/windows_xp_background.jpg')
-    bg = pygame.transform.scale(bg,(WIDTH,HEIGHT))
-    screen.blit(bg, (0,0))
+    #bg = pygame.image.load('resources/windows_xp_background.jpg')
+    #bg = pygame.transform.scale(bg,(WIDTH,HEIGHT))
+    #screen.blit(bg, (0,0))
+
+    screen.fill((0,0,0))
