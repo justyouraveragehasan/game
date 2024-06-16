5ea0362ca684676dcd2407686d3116c1948ad8e5
justyouraveragehasan <hasanali.soda@gmail.com>
2024-06-16 16:34:40 +0100

    TILEMAP BABYgit add .!

--- __main__.py
+++ __main__.py
@@ -38,57 +38,6 @@ def game() -> None:
     while running:
         clock.tick(FPS)
         graphics.draw_game_screen(screen,WIDTH,HEIGHT)
-        
-        isoblock = pygame.image.load("resources/big_test_cube.png").convert_alpha()
-        
-        #this shit works
-        #testblock = isoblock.get_rect()
-        #testblock.center = (200,300)
-
-        #testblock2 = isoblock.get_rect()
-        #testblock2.center = (250,325)
-
-        #
-        #testblock3 = isoblock.get_rect()
-        #testblock3.center = (150,325)
-
-
-        #testblock4 = isoblock.get_rect()
-        #testblock4.center = (200,350)
-
-
-
-        #screen.blit(isoblock,testblock)
-        #screen.blit(isoblock,testblock2)
-        #screen.blit(isoblock,testblock3)
-        #screen.blit(isoblock,testblock4)
-        #block1 = pygame.Rect(WIDTH/2 -200,HEIGHT/2,100,100)
-        
-        coords = [100,100]
-        
-        for i in range(0,15):
-            if i != 6:
-                for j in range(0,8):
-                    screen.blit(isoblock,coords)
-                    coords[1] += 25
-                    coords[0] -= 50
-                    screen.blit(isoblock,coords)
-                    coords[0] += 100
-                    screen.blit(isoblock,coords)
-                    coords[0] -= 50
-                    coords[1] += 25
-                    screen.blit(isoblock,coords)
-                    coords[1] -= 50
-                    coords[0] += 200
-                coords[0] -= 1600
-                if i%2 == 0:
-                    coords[0] += 100
-                else:
-                    coords[0] -= 100
-                coords[1] += 50
-            else:
-                coords[0] += 200
-                coords[1] += 100
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- core/graphics.py
+++ core/graphics.py
@@ -1,7 +1,7 @@
 import pygame
 
 def draw_main_menu(screen,WIDTH,HEIGHT) -> None:
-    bg = pygame.image.load('resources/main_menu_bg.png')
+    bg = pygame.image.load('resources/main_menu_bg.png').convert_alpha()
     bg = pygame.transform.scale(bg,(WIDTH,HEIGHT))
 
     screen.blit(bg, (0,0))
@@ -9,8 +9,24 @@ def draw_main_menu(screen,WIDTH,HEIGHT) -> None:
 
 
 def draw_game_screen(screen,WIDTH,HEIGHT) -> None:
-    #bg = pygame.image.load('resources/windows_xp_background.jpg')
-    #bg = pygame.transform.scale(bg,(WIDTH,HEIGHT))
-    #screen.blit(bg, (0,0))
+    bg = pygame.image.load('resources/windows_xp_background.jpg')
+    bg = pygame.transform.scale(bg,(WIDTH,HEIGHT))
+    screen.blit(bg, (0,0))
 
     screen.fill((0,0,0))
+
+        
+    isoblock = pygame.image.load("resources/big_test_cube.png").convert_alpha()
+    f = open('resources/map.txt')
+    map_data = [[int(c) for c in row] for row in f.read().split('\n')]# I took this from online, it takes each row into an array and takes each tile as an element for a nice 2d array.
+    print(map_data)                                                   # because of the \n check it always has an empty list at the end
+    f.close()
+
+    for y, row in enumerate(map_data):
+            for x, tile in enumerate(row): #enumerate is op, if you use enumerate x is the count and tile is the value so its like for tile in row and for i in range 0,len(row) at the same time
+                if tile > 0:
+                    #pygame.draw.rect(display, (255, 255, 255), pygame.Rect(x * 10, y * 10, 10, 10), 1)
+                    screen.blit(isoblock, (400 + x * 50 - y * 50, 100 + x * 25 + y * 25)) #x offset is 50, y offset is 25 
+                    if tile > 1:
+                        screen.blit(isoblock, (400 + x * 50 - y * 50, 100 + x * 25 + y * 25 -49)) #y offset is 49 pixels for second layer because the top layer would be covered 
+
